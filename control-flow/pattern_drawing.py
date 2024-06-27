e515a04ca71ffa5000067d5ed1886de86866d0f7
eyobed7 <eyobedsebrala083@gmail.com>
2024-06-27 03:40:11 -0700

    added

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,8 +1,8 @@
-pattern = int(input("Enter the size of the pattern:"))
-x=0
-while x < pattern:
-    for i in range(1,pattern ):
-        print("*" * pattern , end="")
-        x+=1
-        print()
+size=int(input("Enter the size of the pattern:")) 
+x = 0
+while x < size:
+    for y in range(size):
+        print("*", end="")
+    print()  
+    x += 1
 
