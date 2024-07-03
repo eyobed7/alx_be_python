5ba506dece02d14fa768fca42b14c9716e932b3b
eyobed7 <eyobedsebrala083@gmail.com>
2024-07-03 13:41:12 -0700

    new change

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,13 @@
 def perform_operation(num1,num2,operation):
-    if operation =="+":
+    if operation =="add":
         return num1 + num2
-    if operation =="*":
+    if operation =="multiply":
         return num1 * num2
-    if operation =="-":
+    if operation =="subtract":
         return num1 - num2
-    if operation =="/":
-        return num1 / num2
+    if operation =="divide":
+        if num2==0:
+            print('no operation')
+        else:
+            return num1 / num2
+        
