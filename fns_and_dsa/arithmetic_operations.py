6cfe03960aec36e0f6949340ba196aea392f0c9d
eyobed7 <eyobedsebrala083@gmail.com>
2024-07-03 13:09:09 -0700

    new file added

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,9 @@
+def perform_operation(num1,num2,operation):
+    if operation =="+":
+        return num1 + num2
+    if operation =="*":
+        return num1 * num2
+    if operation =="-":
+        return num1 - num2
+    if operation =="/":
+        return num1 / num2
