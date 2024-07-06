b01f590992bd27913138db91042de7f65dfdc4f4
eyobed7 <eyobedsebrala083@gmail.com>
2024-07-06 05:57:37 -0700

    now have to work

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,10 +1,10 @@
 from datetime import datetime, timedelta
 
-# Get the current date and time
-current_date = datetime.datetime.now()
-
-# Print the current date and time
-print("Current date and time:", current_date)
+def display_current_datetime():
+    current_date = datetime.datetime.now()
+    # Print the current date and time
+    print("Current date and time:", current_date)
+display_current_datetime()
 
 # Get the number of days to add from the user
 number_of_days = int(input('Enter the number of days to add to the current date: '))
