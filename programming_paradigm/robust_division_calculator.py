a0d9c6e16d3635b9ab8c5df7d9442d3a5e01ff32
eyobed7 <eyobedsebrala083@gmail.com>
2024-07-12 01:00:26 -0700

    there is change in the variables

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,14 +1,14 @@
 def safe_divide(numerator, denominator):
-    try:
-         
+    try:  
        num= float(numerator)
-       den= float(denominator)    
+       den= float(denominator) 
+       C=num / den   
     except ZeroDivisionError :
         print("Error: Cannot divide by zero.")
     except ValueError :
         print("Error: Please enter numeric values only.")
     else:
-        return f"The result of the division is {num / den}"
+        return f"The result of the division is {C}"
 
     
 
